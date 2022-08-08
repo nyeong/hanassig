80640ff4a4054ed9e3ed96488ac258260f255daa
An Nyeong <me@annyeong.me>
2022-08-08 17:22:36 +0900

    Remove generated files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/nyeong/Repos/hanassig/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/nyeong/Repos/hanassig/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/nyeong/Repos/hanassig/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/nyeong/Repos/hanassig/src/css/custom.css'),
-];
--- notes/dev-env/README.md
+++ notes/dev-env/README.md
@@ -1,11 +1,11 @@
 # 개발환경
 
 - 🐚 셸: zsh
-- 📝 텍스트 편집기: [[helix]], VS Code with Dance
-- 🖥터미널 레이아웃: [[tmux]]
-- 📂 파일 탐색기: [[lf]]
+- 📝 텍스트 편집기: helix, VS Code with Dance
+- 🖥터미널 레이아웃: tmux
+- 📂 파일 탐색기: lf
 - 📈 시스템 모니터링: `bottom`, `htop`
-- 버전 관리자: [[git]]
+- 버전 관리자: git
 
 ## 운영체제
 
